224349c696b5582102b78977174bd7ade75c269e
Allison Kao <ahkallison@fb.com>
2017-06-26 11:50:34 -0700

    finished 4/10

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterApplication.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterApplication.java
@@ -1,18 +1,18 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.app.Application;
+import android.content.Context;
+
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowLog;
 import com.raizlabs.android.dbflow.config.FlowManager;
-
-import android.app.Application;
-import android.content.Context;
 
 /*
  * This is the Android application itself and is used to configure various settings
  * including the image cache in memory and on disk. This also adds a singleton
  * for accessing the relevant rest client.
  *
- *     RestClient client = RestApplication.getRestClient();
+ *     TwitterClient client = TwitterApplication.getRestClient();
  *     // use client to send requests to API
  *
  */
@@ -29,7 +29,7 @@ public class TwitterApplication extends Application {
 		TwitterApplication.context = this;
 	}
 
-	public static RestClient getRestClient() {
-		return (RestClient) RestClient.getInstance(RestClient.class, TwitterApplication.context);
+	public static TwitterClient getRestClient() {
+		return (TwitterClient) TwitterClient.getInstance(TwitterClient.class, TwitterApplication.context);
 	}
 }
