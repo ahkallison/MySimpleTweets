07cd47cccf5bf40bc236cb9642a513dc98fe6a11
Allison Kao <ahkallison@fb.com>
2017-07-03 15:58:21 -0700

    finished 5/6

--- app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
@@ -3,7 +3,10 @@ package com.codepath.apps.restclienttemplate;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.fragments.UserTimelineFragment;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -45,10 +48,27 @@ public class ProfileActivity extends AppCompatActivity {
                     user = User.fromJSON(response);
                     // set the title of the ActionBar based on the user information
                     getSupportActionBar().setTitle(user.screenName);
+                    // populate the user headline
+                    populateUserHeadline(user);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         });
+    }
+
+    public void populateUserHeadline(User user) {
+        TextView tvName = (TextView) findViewById(R.id.tvName);
+        TextView tvTagline = (TextView) findViewById(R.id.tvTagline);
+        TextView tvFollowers = (TextView) findViewById(R.id.tvFollowers);
+        TextView tvFollowing = (TextView) findViewById(R.id.tvFollowing);
+        ImageView ivProfileImage = (ImageView) findViewById(R.id.ivProfileImage);
+
+        tvName.setText(user.name);
+        tvTagline.setText(user.tagLine);
+        tvFollowers.setText(user.followersCount + " Followers");
+        tvFollowing.setText(user.followingCount + " Following");
+        // load profile image with Glide
+        Glide.with(this).load(user.profileImageUrl).into(ivProfileImage);
     }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/User.java
@@ -16,6 +16,9 @@ public class User {
     public long uid;
     public String screenName;
     public String profileImageUrl;
+    public String tagLine;
+    public int followersCount;
+    public int followingCount;
 
     // deserialize the JSON
     public static User fromJSON(JSONObject json) throws JSONException {
@@ -26,6 +29,9 @@ public class User {
         user.uid = json.getLong("id");
         user.screenName = json.getString("screen_name");
         user.profileImageUrl = json.getString("profile_image_url");
+        user.tagLine = json.getString("description");
+        user.followersCount = json.getInt("followers_count");
+        user.followingCount = json.getInt("friends_count");
 
         return user;
 
